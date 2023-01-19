caedcf08c73ad51a72725bf6995422b7e63dafe2
Wang55566 <wang6@hawaii.edu>
2023-01-19 12:47:46 -0800

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+router.use('/api', apiRouter);
 //Test Route
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
