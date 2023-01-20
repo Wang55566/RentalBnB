d565c4ca888c9a15b67b8677e7ae71caea39387f
Wang55566 <wang6@hawaii.edu>
2023-01-20 14:23:54 -0800

    Add User model authentication Sequelize scopes and methods

--- backend/db/migrations/20230120210033-create-user.js
+++ backend/db/migrations/20230120210033-create-user.js
@@ -8,8 +8,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    return queryInterface.createTable("Users", {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -42,8 +42,8 @@ module.exports = {
       }
     }, options);
   },
-  down: async (queryInterface, Sequelize) => {
+  async down(queryInterface, Sequelize) {
     options.tableName = "Users";
-    return queryInterface.dropTable(options);
+    await queryInterface.dropTable(options);
   }
 };
