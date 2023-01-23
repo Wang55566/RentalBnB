38e47fa528d9b1d453cf81ea9c20584b587b75ea
Wang55566 <wang6@hawaii.edu>
2023-01-23 13:07:11 -0800

    names

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -32,12 +32,14 @@ module.exports = (sequelize, DataTypes) => {
       }
     }
 
-    static async signup({ username, email, password}) {
+    static async signup({ username, email, password, firstName, lastName}) {
       const hashedPassword = bcrypt.hashSync(password);
       const user = await User.create({
         username,
         email,
-        hashedPassword
+        hashedPassword,
+        firstName,
+        lastName
       });
       return await User.scope('currentUser').findByPk(user.id);
     }
@@ -62,14 +64,14 @@ module.exports = (sequelize, DataTypes) => {
         }
       },
       //Add columns firstName and lastName
-      // firstName: {
-      //   type: DataTypes.STRING,
-      //   allowNull: false,
-      // },
-      // lastName: {
-      //   type: DataTypes.STRING,
-      //   allowNull: false,
-      // },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
       email: {
         type: DataTypes.STRING,
         allowNull: false,
--- backend/db/seeders/20230120212354-demo-user.js
+++ backend/db/seeders/20230120212354-demo-user.js
@@ -14,17 +14,23 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
+        hashedPassword: bcrypt.hashSync('password'),
+        firstName: 'aaa',
+        lastName:'aaaa'
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
+        hashedPassword: bcrypt.hashSync('password2'),
+        firstName: 'bbb',
+        lastName:'bbbb'
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
+        hashedPassword: bcrypt.hashSync('password3'),
+        firstName: 'ccc',
+        lastName:'cccc'
       }
     ], {});
   },
