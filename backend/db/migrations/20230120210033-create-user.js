2b8cf714580fb3daadc2321d82f18b042dc4e01e
Wang55566 <wang6@hawaii.edu>
2023-01-22 23:36:55 -0800

    Names

--- backend/db/migrations/20230120210033-create-user.js
+++ backend/db/migrations/20230120210033-create-user.js
@@ -39,6 +39,12 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
       }
     }, options);
   },
