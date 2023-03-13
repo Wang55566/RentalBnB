2b8f0e181d0fb4a3a859ae78c93ede992d5774c5
Wang55566 <wang6@hawaii.edu>
2023-03-13 00:31:03 -0700

    phase5

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
 const apiRouter = require('./api');
+
 router.use('/api', apiRouter);
 
 // Static routes
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -22,32 +22,18 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 }
 
+// Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+    res.status(201).json({});
   });
 }
-
-//Test whether XSRF-TOKEN is working
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
-
-// Add/Restore a XSRF-TOKEN cookie
-// router.get("/api/csrf/restore", (req, res) => {
-//   const csrfToken = req.csrfToken();
-//   res.cookie("XSRF-TOKEN", csrfToken);
-//   res.status(200).json({
-//     'XSRF-Token': csrfToken
-//   });
-// });
 
 module.exports = router;
